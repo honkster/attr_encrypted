bf953e3e6e5854e16a42d0824b303b2c2906fe26
Dave Willett & Scott Gonyea <dev@truecar.com>
2011-05-12 14:29:11 -0700

    Cleaned up test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,17 +2,9 @@ require 'rubygems'
 require 'bundler/setup'
 
 Bundler.require :development
+Bundler.require :default
 
 require 'test/unit'
 require 'digest/sha2'
-gem 'activerecord', ENV['ACTIVE_RECORD_VERSION'] if ENV['ACTIVE_RECORD_VERSION']
-require 'active_record'
-require 'datamapper'
-require 'sequel'
-require 'mocha'
-
-$:.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-$:.unshift(File.dirname(__FILE__))
-require 'attr_encrypted'
 
 puts "\nTesting with ActiveRecord #{ActiveRecord::VERSION::STRING rescue ENV['ACTIVE_RECORD_VERSION']}"
